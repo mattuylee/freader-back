b71a8b703fe1dd1bd031864f1ca4e6a3d9ecaeaf
mattuy <mattuylee@outlook.com>
2019-08-30 17:15:33 +0800

    fixbug: 在linux上创建已存在的目录出错

--- script/copy-static.js
+++ script/copy-static.js
@@ -65,9 +65,11 @@ function copyDirectory(options = {}) {
             return
         }
         let destPath = path.join(options.dest, file)
-        if (!fs.existsSync(destPath) || fs.statSync(destPath).mtimeMs < fs.statSync(srcPath).mtimeMs) {
-            //复制已修改文件
+        if (!fs.existsSync(destPath)) {
             fs.mkdirSync(path.dirname(destPath), { recursive: true })
+        }
+        if (fs.statSync(destPath).mtimeMs < fs.statSync(srcPath).mtimeMs) {
+            //复制已修改文件
             fs.copyFileSync(srcPath, destPath)
         }
     })
