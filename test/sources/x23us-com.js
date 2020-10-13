fc7e75b6d51fd7166c7319135243fd284c962997
mattuy <mattuylee@outlook.com>
2020-10-13 12:39:45 +0800

    test: 修复X23usCom源测试

--- test/sources/x23us-com.js
+++ test/sources/x23us-com.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai')
-const { RemoteSource, ResourceInformation } = require('../../bin/domain/resource-info')
+const { SourceLiteral, ResourceInformation } = require('../../bin/domain/resource-info')
 const { instance: dingdian } = require('../../bin/service/crawling/x23us-com')
 
 describe("顶点（x23us）数据源测试", function () {
@@ -9,7 +9,7 @@ describe("顶点（x23us）数据源测试", function () {
   }).timeout(0)
 
   it("测试获取书籍信息", async function () {
-    const book = await dingdian.detail('63f44e872b6faa293ccac88c40809543', new ResourceInformation(RemoteSource.X23usCom, '503/'))
+    const book = await dingdian.detail('63f44e872b6faa293ccac88c40809543', new ResourceInformation(SourceLiteral.X23usCom, '503/'))
     expect(book.name).to.equal('诡秘之主', "获取书籍名称失败")
     expect(book.author).to.equal('爱潜水的乌贼', "获取书籍作者失败")
     expect(book.detailPageInfo).have.property('data').that.is.an('string', "获取详情页信息失败")
@@ -19,7 +19,7 @@ describe("顶点（x23us）数据源测试", function () {
 
   it("测试获取书籍目录", async function () {
     const catalog = await dingdian.catalog('63f44e872b6faa293ccac88c40809543', {
-      source: RemoteSource.X23usCom,
+      source: SourceLiteral.X23usCom,
       data: '503/'
     })
     expect(catalog).to.be.an('array', "获取书籍目录失败").that.lengthOf.above(0, "书籍目录信息为空")
@@ -29,7 +29,7 @@ describe("顶点（x23us）数据源测试", function () {
     const chapter = await dingdian.chapter(
       '63f44e872b6faa293ccac88c40809543',
       '4dc7ec75ae24572889678010ac57dc92',
-      new ResourceInformation(RemoteSource.X23usCom, '503/49020.html')
+      new ResourceInformation(SourceLiteral.X23usCom, '503/49020.html')
     )
     expect(chapter.content).lengthOf.above(0, "获取章节内容失败")
   }).timeout(0)
