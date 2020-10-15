ede9996116ade4612abb3e39a84617bb9a0705bd
mattuy <mattuylee@outlook.com>
2020-10-15 21:16:21 +0800

    test: 重写测试脚本

--- script/test.js
+++ script/test.js
@@ -1,1 +1,59 @@
-require('../test/index')
+const childProcess = require('child_process')
+  , path = require('path')
+  , help = `
+【测试】
+测试接口：npm test api
+运行本地服务器并测试接口：npm test api --run
+测试所有数据源：npm test sources
+测试特定数据源：npm test <数据源名称>
+可用的数据源（不区分大小写）：
+* Qidian
+* X23usCom
+`.trim()
+  , sources = { qidian: 'qidian.js', x23uscom: 'x23us-com.js' }
+  , args = process.argv.slice(2)
+
+if (args.length === 0) {
+  console.log(help)
+  return
+}
+
+const target = args[0]
+if (/^-?-?help$/.test(target)) {
+  console.log(help)
+}
+else if (target.toLowerCase() === 'api') {
+  let shouldRun = false
+  if (/^--?run$/.test(args[1])) {
+    shouldRun = true
+  }
+  else if (args.length > 1) {
+    console.error("未识别的参数：" + args[1])
+    return
+  }
+  require('../test/index').run(shouldRun)
+}
+else if (target.toLowerCase() === 'sources') {
+  if (args.length > 1) {
+    console.error("未识别的参数：" + args[1])
+  }
+  childProcess.spawn(
+    'npx',
+    ['mocha', path.resolve(__dirname, '../test/sources', '*.js')],
+    { shell: true, stdio: 'inherit' }
+  )
+}
+else {
+  const targets = []
+  for (const source of args) {
+    if (sources[source.toLowerCase()]) {
+      targets.push(sources[source.toLowerCase()])
+    }
+    else {
+      console.error("参数错误：找不到数据源 " + source)
+      return
+    }
+  }
+  const files = targets.map(t => path.resolve(__dirname, '../test/sources', t))
+  childProcess.spawn('npx', ['mocha', ...files], { shell: true, stdio: 'inherit' })
+}
--- test/index.js
+++ test/index.js
@@ -8,99 +8,93 @@ const globalConfig = require('../bin/util/index').globalConfig
 
 var testConfig
 var token
-var argus = {
-    run: false
-} //运行参数
-if (process.argv.length > 2) {
-    for (let i = 2; i < process.argv.length; ++i) {
-        if (/^--?run$/.test(process.argv[i])) { argus.run = true }
-    }
+
+async function run(shouldRun) {
+  if (shouldRun) {
+    const app = require('../bin/app')
+    await app.run()
+  }
+  logger.trace('---测试开始---')
+  runTest().then(() => {
+    logger.info('---测试结束---')
+    process.exit(0)
+  }).catch(e => {
+    logger.error('---测试失败---')
+    logger.error(e.message ? e.message : e)
+  })
 }
-(async function () {
-    if (argus.run) {
-        const app = require('../bin/app')
-        await app.run()
-    }
-    logger.trace('---测试开始---')
-    runTest().then(() => {
-        logger.info('---测试结束---')
-        process.exit(0)
-    }).catch(e => {
-        logger.error('---测试失败---')
-        logger.error(e.message ? e.message : e)
-        process.exit()
-    })
-})()
 
 /** 执行测试用例 */
 async function runTest() {
-    try {
-        testConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, './test-config.json')).toString('utf-8'))
-    }
-    catch {
-        logger.fatal('Failed to run test: test config file not found.')
-        return
-    }
-    let hostname
-    if (testConfig.host && testConfig.port) {
-        hostname = testConfig.host + ':' + testConfig.port + testConfig.basePath
-    }
-    else {
-        hostname = 'localhost:' + (testConfig.port ? testConfig.port : globalConfig.port) + testConfig.basePath
-    }
-    if (hostname.endsWith('/')) { hostname = hostname.slice(0, hostname.length - 1) }
-    //执行登录，获取凭证
-    await superAgent.post(hostname + '/user/login').type('form').send({
-        uid: testConfig.user,
-        pwd: testConfig.password
-    }).then(async res => {
-        assertResult(res)
-        //执行测试用例
-        await runTests(testConfig.testCases, hostname)
-    })
+  try {
+    testConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, './test-config.json')).toString('utf-8'))
+  }
+  catch {
+    logger.fatal('Failed to run test: test config file not found.')
     return
+  }
+  let hostname
+  if (testConfig.host && testConfig.port) {
+    hostname = testConfig.host + ':' + testConfig.port + testConfig.basePath
+  }
+  else {
+    hostname = 'localhost:' + (testConfig.port ? testConfig.port : globalConfig.port) + testConfig.basePath
+  }
+  if (hostname.endsWith('/')) { hostname = hostname.slice(0, hostname.length - 1) }
+  //执行登录，获取凭证
+  await superAgent.post(hostname + '/user/login').type('form').send({
+    uid: testConfig.user,
+    pwd: testConfig.password
+  }).then(async res => {
+    assertResult(res)
+    //执行测试用例
+    await runTests(testConfig.testCases, hostname)
+  })
+  return
 }
 async function runTests(testcase, route = '') {
-    if (!testcase || typeof testcase != 'object') { return }
-    if (testcase.title) {
-        logger.trace('【测试】' + testcase.title)
+  if (!testcase || typeof testcase != 'object') { return }
+  if (testcase.title) {
+    logger.trace('【测试】' + testcase.title)
+  }
+  if (testcase.type == 'route') {
+    if (!testcase.title) { testcase.title = '' }
+    for (let method in testcase) {
+      if (method == 'form') {
+        logger.trace(`[POST-FORM]${route}`)
+        assertResult(await superAgent.post(route).set('token', token).type('form').send(testcase.form))
+        continue
+      }
+      else if (method != 'get' && method != 'post' && method != 'put' && method != 'delete') {
+        continue
+      }
+      logger.trace(`[${method.toUpperCase()}]${route}`)
+      let sendMethod
+      if (method == 'get' || method == 'delete') {
+        sendMethod = 'query'
+      }
+      else { sendMethod = 'send' }
+      //发起请求，并对结果进行正常性断言
+      assertResult(await superAgent[method](route).set('token', token)[sendMethod](testcase[method]))
     }
-    if (testcase.type == 'route') {
-        if (!testcase.title) { testcase.title = '' }
-        for (let method in testcase) {
-            if (method == 'form') {
-                logger.trace(`[POST-FORM]${route}`)
-                assertResult(await superAgent.post(route).set('token', token).type('form').send(testcase.form))
-                continue
-            }
-            else if (method != 'get' && method != 'post' && method != 'put' && method != 'delete') {
-                continue
-            }
-            logger.trace(`[${method.toUpperCase()}]${route}`)
-            let sendMethod
-            if (method == 'get' || method == 'delete') {
-                sendMethod = 'query'
-            }
-            else { sendMethod = 'send' }
-            //发起请求，并对结果进行正常性断言
-            assertResult(await superAgent[method](route).set('token', token)[sendMethod](testcase[method]))
-        }
-    } //路由级测试用例
-    else if (testcase.type == 'module' || !testcase.type) {
-        for (const key in testcase) { await runTests(testcase[key], route + key) }
-        return
-    } //模块级测试用例
+  } //路由级测试用例
+  else if (testcase.type == 'module' || !testcase.type) {
+    for (const key in testcase) { await runTests(testcase[key], route + key) }
+    return
+  } //模块级测试用例
 }
 
 //结果断言
 function assertResult(res) {
-    if (!res) { return }
-    assert.strictEqual(res.statusCode, 200)
-    assert.strictEqual(!res.body.code, true)
-    if (!res.body.error) { res.body.error = null }
-    assert.strictEqual(res.body.error, null)
-    if (res.body.token) {
-        token = res.body.token
-        logger.debug('token changed: ', token)
-    }
+  if (!res) { return }
+  assert.strictEqual(res.statusCode, 200)
+  assert.strictEqual(!res.body.code, true)
+  if (!res.body.error) { res.body.error = null }
+  assert.strictEqual(res.body.error, null)
+  if (res.body.token) {
+    token = res.body.token
+    logger.debug('token changed: ', token)
+  }
 }
+
+module.exports = { run }
